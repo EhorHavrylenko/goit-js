fd862c9e6f8c782f756038a14db2083bb0999e5c
Egor Bass <egorbass@MacBook-Pro-Egor.local>
2020-06-30 12:36:06 +0300

    fix 4

---
+++ hw-01/js/js-practice.js
@@ -0,0 +1,40 @@
+// поиск в массиве
+
+// const staff = ["Oleg", "Peta", "Sasha", "Ola"];
+// const query = "Ola";
+
+// const result = staff.includes(query)
+//   ? "присутствует на работе"
+//   : "отсутствует на работе";
+
+// console.log(result);
+
+//
+
+// удаление элемента из массива
+
+// const cards = ["card 1", "card 2", "card 3", "card 4"];
+// console.log(cards);
+
+// const result = cards.splice(1, 2);
+
+// console.log(cards);
+
+//
+// удаление элемента из массива по индексу / добавление нового элемента / обнвление
+
+// const cards = ["card 1", "card 2", "card 3", "card 4"];
+
+// const cardToRemove = "card 3";
+// const index = cards.indexOf(cardToRemove);
+
+// cards.splice(index, 1);
+
+// cards.splice(2, 0, "hello");
+
+// cards.splice(3, 1, "card 4-modded");
+
+// console.log(cards);
+
+//
+//
