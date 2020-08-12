5e79502cc3d64755d91f2681533072f73adba209
EhorHavrylenko <62027112+EhorHavrylenko@users.noreply.github.com>
2020-08-12 12:41:16 +0300

    Add files via upload

---
+++ hw-05/js/task-03.js
@@ -0,0 +1,35 @@
+class Storage {
+  constructor([...items]) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    return this.items.push(item);
+  }
+
+  removeItem(item) {
+    const elToRemove = item;
+    const indexElToRemove = this.items.indexOf(elToRemove)
+    this.items.splice(indexElToRemove, 1);
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор",
+]);
+
+const items = storage.getItems();
+console.table(items);
+
+storage.addItem("Дроид");
+console.table(storage.items);
+
+storage.removeItem("Пролонгер");
+console.table(storage.items);
---
+++ hw-05/js/task-05.js
@@ -0,0 +1,74 @@
+class Car {
+  static getSpecs(car) {
+    return console.log(
+      `MaxSpeed: ${car.maxSpeed}, Speed: ${car.speed}, isOn: ${car.isOn}, Distance: ${car.distance}, Price: ${car.price},`
+    );
+  }
+
+  constructor(car) {
+    (this.speed = 0),
+      (this._price = car.price),
+      (this.maxSpeed = car.maxSpeed),
+      (this.isOn = false),
+      (this.distance = 0);
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(newPrice) {
+    this._price = newPrice;
+  }
+
+  turnOn() {
+    this.isOn = true;
+  }
+
+  turnOff() {
+    (this.isOn = false), (this.speed = 0);
+  }
+
+  accelerate(value) {
+    let resultSpeed = this.speed + value;
+
+    if (resultSpeed < this.maxSpeed) {
+      this.speed = resultSpeed;
+    }
+  }
+
+  decelerate(value) {
+    let decSpeed = this.speed - value;
+
+    if (decSpeed > 0) {
+      this.speed = decSpeed;
+    }
+  }
+
+  drive(hours) {
+    if (this.isOn) {
+      this.distance = hours * this.speed;
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+
+mustang.price = 4000;
+console.log(mustang.price);  // 4000
